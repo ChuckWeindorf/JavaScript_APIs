4066b38601d8736f89cc1a68986e09b6308ef018
ChuckWeindorf <charles.weindorf@mudsox.com>
2024-01-30 15:28:17 -0500

    First Try

--- app.js
+++ app.js
@@ -2,3 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+var vbtnSearch = document.getElementById("submitSearch");
+var vtxtSearch = document.getElementById("searchWord");
+var vimgShow = document.getElementById("imageItself");
+var vtxtFeedback = document.getElementById("feedback");
+
+// Exercise 2 Section
+vbtnSearch.addEventListener("click", getGif);
+
+function getGif() {
+    
+  fetch("https://api.giphy.com/v1/gifs/translate?api_key=6Cqmx8A4vpZoFBI2lcSso52Bj2qbrERL&s=" 
+    + vtxtSearch.value, { mode: "cors" })
+    // Exercise 3 Section
+    .then((res) => res.json().then((data) => 
+      {vimgShow.src = data.data.images.original.url;
+    //Exercise 4 Section
+        vtxtFeedback.textContent = "Found a GIF for you";
+        vtxtSearch.value = "";
+      }))
+    .catch((err) => { vtxtFeedback.textContent = err.message});
+};
