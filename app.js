ec34f80a5f11915ca1c885f70556119b3e7e6550
ChuckWeindorf <charles.weindorf@mudsox.com>
2024-01-31 10:45:12 -0500

    Tweak

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ console.log("EXERCISE 1:\n==========\n");
 var vbtnSearch = document.getElementById("submitSearch");
 var vtxtSearch = document.getElementById("searchWord");
 var vimgShow = document.getElementById("imageItself");
+//this also works to find the parent and drill down
+//var vimgShow = document.querySelector("#imageContainer > img");
 var vtxtFeedback = document.getElementById("feedback");
 
 // Exercise 2 Section
@@ -15,11 +17,13 @@ function getGif() {
   fetch("https://api.giphy.com/v1/gifs/translate?api_key=6Cqmx8A4vpZoFBI2lcSso52Bj2qbrERL&s=" 
     + vtxtSearch.value, { mode: "cors" })
     // Exercise 3 Section
-    .then((res) => res.json().then((data) => 
-      {vimgShow.src = data.data.images.original.url;
+    .then((res) => res.json()
+      .then((data) => 
+        {vimgShow.src = data.data.images.original.url;
     //Exercise 4 Section
         vtxtFeedback.textContent = "Found a GIF for you";
         vtxtSearch.value = "";
       }))
-    .catch((err) => { vtxtFeedback.textContent = err.message});
+    .catch((err) => {vtxtFeedback.textContent = err.message;
+                     vimgShow.src=""});
 };
